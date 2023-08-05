e2321052c3eea55fe85a609dfd6813fdae1924a9
Atharv Deshmukh <atharvdeshmukh@Atharvs-MacBook-Air.local>
2023-08-05 13:13:29 -0700

    error handling

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -6,6 +6,42 @@ type Feedback = {
 }
 
 export async function POST(request: Request) {
+  try {
+      const data: Feedback = await request.json();
+      console.log('data: ', data);
+
+      const { email } = data;
+
+      await (async () => {
+          try {
+              const auth = new google.auth.JWT({
+                  email: process.env.GOOGLE_CLIENT_EMAIL,
+                  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+                  scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+              });
+
+              const sheet = google.sheets("v4");
+              await sheet.spreadsheets.values.append({
+                  spreadsheetId: process.env.GOOGLE_SHEET_ID,
+                  auth: auth,
+                  range: "Sheet1",
+                  valueInputOption: "USER_ENTERED",
+                  requestBody: {
+                      values: [[data.email]]
+                  }
+              });
+          } catch (error) {
+              console.error("Error while appending to Google Sheets:", error);
+          }
+      })();
+
+      return NextResponse.json({ email });
+  } catch (error) {
+      console.error("Error while processing request:", error);
+      return NextResponse.error();
+  }
+}
+/*export async function POST(request: Request) {
     const data: Feedback = await request.json()
     console.log('data: ', data)
 
@@ -32,4 +68,4 @@ export async function POST(request: Request) {
       })()
       
     return NextResponse.json({email})
-}
+}*/
