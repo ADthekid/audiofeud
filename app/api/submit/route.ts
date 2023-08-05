0128e5f7d3c0099a4ec7b80499acb291482aad58
Atharv Deshmukh <atharvdeshmukh@Atharvs-MacBook-Air.local>
2023-08-05 12:55:15 -0700

    fixed async function

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -11,25 +11,28 @@ export async function POST(request: Request) {
 
     const {email} = data
 
-    ;(async () => {
-        const auth = new google.auth.JWT({
-          email: process.env.GOOGLE_CLIENT_EMAIL,
-          key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-          scopes: ["https://www.googleapis.com/auth/spreadsheets"]
-        })
-        console.log(process.env.GOOGLE_SHEET_ID)
-        console.log(auth)
-        const sheet = google.sheets("v4")
-        await sheet.spreadsheets.values.append({
-          spreadsheetId: process.env.GOOGLE_SHEET_ID,
-          auth: auth,
-          range: "Sheet1",
-          valueInputOption: "USER_ENTERED",
-          requestBody: {
-            values: [[data.email]]
-          }
-        })
-      })()
-      
+    const auth = new google.auth.JWT({
+      email: process.env.GOOGLE_CLIENT_EMAIL,
+      key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+    })
+    const sheet = google.sheets("v4")
+    
+    async function logEmail() {
+      const result = await sheet.spreadsheets.values.append({
+        spreadsheetId: process.env.GOOGLE_SHEET_ID,
+        auth: auth,
+        range: "Sheet1",
+        valueInputOption: "USER_ENTERED",
+        requestBody: {
+          values: [[data.email]]
+        }
+      })
+
+      console.log(result)
+    }
+
+    logEmail()
+    
     return NextResponse.json({email})
 }
